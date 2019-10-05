62e6c55369a6f8bbe0506f20eadb60d91ab8547e
soamya.agrawal <soamya.agrawal@flipkart.com>
2019-10-05 23:34:35 +0530

    adding full name of author and removing unnecessary comments

--- CUR.java
+++ CUR.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package IRRecommenderSystem;
 
 import Jama.EigenvalueDecomposition;
@@ -16,7 +11,7 @@ import java.util.Random;
 
 /**
  *
- * @author Soamya
+ * @author Soamya Agrawal
  */
 public class CUR {
 
