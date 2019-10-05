dc67f9b612de74188ffbc91816a4c10a882e4514
soamya.agrawal <soamya.agrawal@flipkart.com>
2019-10-06 01:51:02 +0530

    adding slf4j dependency for using logs instead of sout

--- recommender_system/src/main/java/CompareSVDAndCUR.java
+++ recommender_system/src/main/java/CompareSVDAndCUR.java
@@ -1,6 +1,9 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.io.Serializable;
 
+@Slf4j
 public class CompareSVDAndCUR implements Serializable {
     public static void main(String[] args) {
         SVD svd = new SVD();
@@ -15,19 +18,18 @@ public class CompareSVDAndCUR implements Serializable {
 
                 long endTime = System.currentTimeMillis();
                 long totalTime = endTime - startTime;
-                System.out.println("SVD time is " + totalTime);
-                System.out.println("SVDError " + i + "rows and " + "1000 columns " + svdError);
+                log.info("SVD time is " + totalTime);
+                log.info("SVDError " + i + "rows and " + "1000 columns " + svdError);
 
                 long startTime1 = System.currentTimeMillis();
                 double curError = cur.getCURError(i);
                 long endTime1 = System.currentTimeMillis();
                 long totalTime1 = endTime1 - startTime1;
-                System.out.println("CUR time is " + totalTime1);
-                System.out.println("CURError " + i + "rows and " + i + "columns " + curError);
+                log.info("CUR time is " + totalTime1);
+                log.info("CURError " + i + "rows and " + i + "columns " + curError);
 
 
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
 
